011b822ed7d71af4db54d9cbcc94bc1c025310bf
VA_Al <VA_Al@DESKTOP-RRHDD81>
2021-12-12 16:36:54 +0300

    Добавьте файлы проекта.

---
+++ Lesson 22/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_22
+{
+    class Program
+    // Сформировать массив случайных целых чисел(размер задается пользователем).
+    // Вычислить сумму чисел массива и максимальное число в массиве.
+    // Реализовать решение  задачи с  использованием механизма  задач продолжения.
+    {
+        //объявление переменных
+        static int[] massivM;
+        static int w;
+        static int summa;
+        static int max;
+        static void Main(string[] args)
+        {
+            //Запрос размера массива
+            Console.WriteLine("Задайте размер массива");
+            w = Convert.ToInt32(Console.ReadLine());
+            summa = 0;
+            max = 0;
+            massivM = new int[w];
+            Random random = new Random();
+            //заполнение массива случайными числами
+            for (int i = 0; i < massivM.Length; i++)
+            {
+                massivM[i] = random.Next(100);
+                Console.WriteLine($"Значение индекса = {i}\nЗначение элемента = {massivM[i]}");
+            }
+            //Создание задач
+            Action action = new Action(SummaMassiva);
+            Action<Task> action2 = new Action<Task>(MaxMassiva);
+            Task task1 = new Task(action);
+            Task task2 = task1.ContinueWith(action2);
+            task1.Start();
+            task2.Wait();
+            Console.WriteLine($"Сумма чисел равна {summa}\nМаксимальное число равно {max}" );
+            
+
+            Console.ReadKey();
+        }
+        //создание методов нахождения суммы и максимума
+        static void SummaMassiva()
+        {
+            summa = massivM.Sum();
+            
+        }
+        static void MaxMassiva(Task task)
+        {
+            max = massivM.Max();
+            
+        }
+
+    }
+
+
+}
---
+++ Lesson 22/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lesson 22")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lesson 22")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ec1ab3d3-0b94-4f07-aa9d-b36eba2cf5b2")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
